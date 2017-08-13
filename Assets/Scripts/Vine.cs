8aea1bda9ab2d3e0aa637219be4d2c2b870cef6e
unknown <courses@gregiscool.com>
2017-08-13 12:37:59 -0700

    Adding new scripts folder and intiail scripts

---
+++ Assets/Scripts/Vine.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Vine : MonoBehaviour {
+
+    public float angle;
+    public float speed;
+    public Vector3 direction;
+
+    private Quaternion qStart;
+    private Quaternion qEnd;
+
+	// Use this for initialization
+	void Start () {
+        qStart = Quaternion.AngleAxis(angle, direction);
+        qEnd = Quaternion.AngleAxis(-angle, direction);
+	}
+
+	// Update is called once per frame
+	void Update () {
+        this.transform.rotation = Quaternion.Lerp(qStart, qEnd, (Mathf.Sin(Time.time * speed) + 1.0f) / 2.0f);
+	}
+}
