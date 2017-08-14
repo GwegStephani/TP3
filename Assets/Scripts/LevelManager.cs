fe7a3fee899982793b957d6367830f210453198b
badical <c.stephen.palacio@gmail.com>
2017-08-13 19:15:41 -0700

    Added main menu scene and levelmanager script
    +music
    +added to build settings

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel (string name)
+    {
+        Debug.Log ("Level load requested for: " + name);
+        Application.LoadLevel(name);
+    }
+
+    //no argument needed since we don't quit to a specific place
+    public void QuitRequest ()
+    {
+        Debug.Log ("Game quit requested via: " + name);
+        Application.Quit();
+    }
+
+}
