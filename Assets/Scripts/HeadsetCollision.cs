35be516e2c71bb2cd836c4caaeefc261c62043de
unknown <courses@gregiscool.com>
2017-08-13 18:40:32 -0700

    added headsetcollision script and updated level1 to make it work

---
+++ Assets/Scripts/HeadsetCollision.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+[RequireComponent(typeof(VRTK_HeadsetCollision))]
+public class HeadsetCollision : MonoBehaviour {
+
+    [Tooltip("The VRTK Headset Collision script to use when determining headset collisions. If this is left blank then the script will need to be applied to the same GameObject.")]
+    public VRTK_HeadsetCollision headsetCollision;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    protected virtual void OnEnable() {
+        headsetCollision = (headsetCollision != null ? headsetCollision : GetComponentInChildren<VRTK_HeadsetCollision>());
+
+        headsetCollision.HeadsetCollisionDetect += new HeadsetCollisionEventHandler(OnHeadsetCollisionDetect);
+        headsetCollision.HeadsetCollisionEnded += new HeadsetCollisionEventHandler(OnHeadsetCollisionEnded);
+    }
+
+    protected virtual void OnDisable() {
+        headsetCollision.HeadsetCollisionDetect -= new HeadsetCollisionEventHandler(OnHeadsetCollisionDetect);
+        headsetCollision.HeadsetCollisionEnded -= new HeadsetCollisionEventHandler(OnHeadsetCollisionEnded);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void HandleResetHit() {
+        // get start object
+        GameObject startObject = GameObject.Find("Start");
+
+        // get start location
+        Vector3 startLocation = startObject.transform.position;
+
+        // teleport to start location
+        //playAreaTransform.position = startLocation;
+        VRTK_BasicTeleport basicTPScript = this.GetComponent<VRTK_BasicTeleport>();
+        basicTPScript.ForceTeleport(startLocation);
+
+    }
+
+    protected virtual void OnHeadsetCollisionDetect(object sender, HeadsetCollisionEventArgs e) {
+        // get name of object collided with
+        GameObject collidedObject = e.collider.gameObject;
+
+        switch(collidedObject.name) {
+        case "Reset":
+            HandleResetHit();
+            break;
+        case "Goal":
+            HandleResetHit();
+            break;
+        default:
+            break;
+        }
+    }
+
+    protected virtual void OnHeadsetCollisionEnded(object sender, HeadsetCollisionEventArgs e) {
+        
+    }
+}
